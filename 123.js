ac0a9e681951598006dc825e5b9a09ce96f97b0c
imjd37 <jitendrapratp37@gmail.com.com>
2023-08-10 08:57:24 +0530

    added

---
+++ 123.js
@@ -0,0 +1,10 @@
+
+const arr = ["java", "html", "cpp", "ruby", "css"]
+
+// arr.forEach((item) => console.log(item))
+
+function printMe(item){
+    console.log(item)
+}
+
+arr.forEach(printMe)
--- script.js
+++ script.js
@@ -6,3 +6,10 @@ var settingsmenu = document.querySelector(".settingmenu");
 function settingsmenutoggle(){
     settingsmenu.classList.toggle("settingmenuheight");
 }
+
+
+document.getElementById("darkmode").onclick = function(){
+    document.body.style.backgroundColor = "#414141"
+   
+}
+
